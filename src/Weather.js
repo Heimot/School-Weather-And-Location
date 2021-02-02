5909122cda76074371a00e5a5d607c50e2e3f4bf
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-02-02 13:43:55 +0200

    Initial

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 const API_URL = "https://api.openweathermap.org/data/2.5/onecall?"; /*lat={lat}&lon={lon}&exclude={part}&appid={API key}*/
 const ICON_URL = "http://openweathermap.org/img/wn/";
-const API_KEY = "";
+const API_KEY = "fb445191f13df590248ddab37a592362";
 
 function Weather(props) {
     const [temp, setTemp] = useState(0);
@@ -14,18 +14,19 @@ function Weather(props) {
     useEffect(() => {
         const url = `${API_URL}lat=${props.lat}&lon=${props.lng}&units=metric&appid=${API_KEY}`;
         fetch(url)
-        .then(res => res.json())
-        .then((result) => {
-            if(result.current !== undefined) {
-                setTemp(result.current.temp);
-                setSpeed(result.current.wind_speed);
-                setDirection(result.current.wind_deg);
-                setDescription(result.current.weather[0].description);
-                setIcon(ICON_URL + result.current.weather[0].icon + '@2x.png');
-            } else {
-                alert('Could not read weather information')
-            }
-        })
+            .then(res => res.json())
+            .then((result) => {
+                if (result.current !== undefined) {
+                    setTemp(result.current.temp);
+                    setSpeed(result.current.wind_speed);
+                    setDirection(result.current.wind_deg);
+                    setDescription(result.current.weather[0].description);
+                    setIcon(ICON_URL + result.current.weather[0].icon + '@2x.png');
+                } else {
+                    alert('Could not read weather information')
+                }
+            })
+        /* Propsit poistaa varoituksen consolista ja ei tuota haittaa tämmöisessä sovelluksessa, sillä tieto ei vaihdu enää uudelleen */
     }, [props])
 
     return (
@@ -34,7 +35,7 @@ function Weather(props) {
             <p>{temp} C&#176;</p>
             <p>{speed} m/s {direction} degrees</p>
             <p>{description}</p>
-            <img src={icon} alt=""/>
+            <img src={icon} alt="" />
         </div>
     )
 }
